81d32a0aa7f14121e1df91a7eebca491965ef09d
DiegoRibeiroo <diego09022004@gmail.com>
2026-07-20 01:01:40 -0300

    feat: add About component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { Disclaimer } from './components/disclaimer/disclaimer';
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero, Disclaimer],
+  imports: [RouterOutlet, Navbar, Hero, Disclaimer, About],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
